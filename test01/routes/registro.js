73994c5c4a4ac128c47c9417b5a3be24d37dc0db
Jorge <jorgeelias437@gmail.com>
2021-10-26 23:16:44 -0600

    agregando mysql

--- test01/routes/registro.js
+++ test01/routes/registro.js
@@ -3,4 +3,30 @@ var axios= require('axios');
 var xml2js= require('xml2js');
 var router=express.Router();
 
-const oracledb= require('oracledb');
+//const oracledb= require('oracledb');
+
+async function getDataFromDatabaseMysql(){
+    return new Promise((resolve, reject)=>{
+        var mysql= require('mysql');
+        var connection= mysql.createConnection({
+            host: 'localhost',
+            user: 'root',
+            password: '',
+            database: 'baseDatos'
+        });
+        connection.connect();
+        connection.query('SELECT * FROM proveedores', function(error, result, fields){
+            if(error) throw error;
+            resolve(result);
+        });
+        connection.end();
+    });
+}
+
+
+router.get('/consulta',keycloak.protect(), async function(req, res){
+    const resultFromDatabase= await getDataFromDatabaseMysql();
+    res.send({result:resultFromDatabase});
+});
+
+module.exports= router;
